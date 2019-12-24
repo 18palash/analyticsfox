935d7e7939b487c160e2dfc814eff17af2a94380
Palash Khobragade <18palash@gmail.com>
2019-12-24 14:04:27 +0530

    validation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-//import Login from './components/Login';
+
+import Login from './components/Login';
 import Register from './components/Register';
+
 //import Home from './components/Home';
 //import Edit from './components/Edit';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  return(
+    <div>
+      <h1> React page </h1>
       
-        <h1>React page</h1>
+    
+        <Login /> 
         <Register />
-        
-        
     </div>
-  );
+  )
 }
-
 export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,24 +1,59 @@
 import React , {Component} from 'react';
 
+
+
 class Login extends Component {
     constructor(props){
         super(props);
-       
+        
         this.state = {  
             username : "",
-            password : ""
+            password : "",
+            usernameError: "",
+            passwordError: ""            
         }
     }
+
+    validate = () => {
+        let usernameError = "";
+        let passwordError = "";
+        // let passwordError = "";
+    
+        if (!this.state.username) {
+          usernameError = "name cannot be blank";
+        }
+        
+        if (!this.state.password) {
+            passwordError = "name cannot be blank";
+          }
+          
+        if (!this.state.username.includes("@")) {
+          usernameError = "invalid email";
+        }
+    
+        if (usernameError || passwordError) {
+          this.setState({ usernameError, passwordError });
+          return false;
+        }
+    
+        return true;
+      };
+    
 
     handlechangeall = (event) =>{
         this.setState ( { [event.target.name] :event.target.value  } )
     }
    
     handlelogin = (event) => {
+        const isValid = this.validate();
+        if (isValid) {
+            console.log(this.state);
+            alert( JSON.stringify(this.state));
+             
+        }
+            alert( JSON.stringify(this.state));
         
-        alert( JSON.stringify(this.state));
-        
-        event.preventDefault();
+            event.preventDefault();
     }
     
     render(){
@@ -31,14 +66,22 @@ class Login extends Component {
                     <input type="text" name="username"
                      value= {this.state.username} onChange={this.handlechangeall} />
                     <br/>
+                    <div style={{ fontSize: 12, color: "red" }}>
+                        {this.state.usernameError}
+                    </div>
+                    <br/>
 
                     <label>Password</label>
                     <input type="password" name="password" 
                      value= {this.state.password} onChange={this.handlechangeall} />
                     <br/>
-
+                    <div style={{ fontSize: 12, color: "red" }}>
+                        {this.state.passwordError}
+                    </div>
+                    <br/>
                     <input type="submit" value="Login" name="submit"/>
                 </form>
+                    <p>Not a User ? Register here</p>
             </div>
         )
 
